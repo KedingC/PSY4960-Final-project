68479ba59758a00bca9c8ff0d39ebf358ad42cd9
KedingC <98502007+KedingC@users.noreply.github.com>
2022-04-19 10:00:58 -0500

    Add files via upload

---
+++ Description.R
@@ -0,0 +1,17 @@
+Assignment9:
+Part1:
+  Final project workflow:
+  1. create hypothesis
+  2. Preparation to the data
+    a. import the data
+    b. rename the columns we want
+    c. filter the columns we want and create the dataset related to our hypothesis
+    d. relevel and change the class of data into the way we want
+  3. Analysis of the data
+    a. summary the data
+    b. create graph related to the summarization
+    c. run relative statistic test
+    d. create graph related to statistic test
+  4. Write summarization 
+  
+  
